e4b583d44ee0c51814694daf368a3092341a0a7a
hungngo <qhung.bm0409@gmail.com>
2023-10-24 23:02:51 +0700

    update

--- script/sh/deploy.sh
+++ script/sh/deploy.sh
@@ -28,13 +28,15 @@ forge script script/forge/Deployer.s.sol:DeployScript \
     --private-key "${OWNER_PRIVATE_KEY}" \
     --broadcast
 
-if [[ "$VERIFY_CONTRACTS" == "true" && -z "$CONTRACTS_VERIFY_KEY" ]]; then
-    --rpc-url "${RPC_URL}"
-    --verify \
-    --"${CONTRACTS_VERIFY_KEY}" \
-    -vvvv \
-    --via-ir
-fi
+if [[ "$VERIFY_CONTRACTS" == "true" && -n "$CONTRACTS_VERIFY_URL" ]]; then
+    address=$(jq -r \
+        '.transactions[0].contractAddress' \
+        ./broadcast/AssetRegistryDeployer.s.sol/$CHAIN_ID/run-latest.json)
 
+    forge verify-contract  \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
+    --verifier-url $CONTRACTS_VERIFY_URL $address AssetAddressesProviderRegistry \
+    --watch
+fi
 # Generate deployment file.
 ./script/sh/gen-deployment-file.sh "$DEPLOYMENTS_FOLDER" "SimpleOracle" "Deployer" "simple_oracle"
