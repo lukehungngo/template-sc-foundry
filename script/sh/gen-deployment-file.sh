0d9256d01963c45ebf81e779a9bc7130507b684a
hungngo <qhung.bm0409@gmail.com>
2023-10-24 23:06:20 +0700

    update

--- script/sh/gen-deployment-file.sh
+++ script/sh/gen-deployment-file.sh
@@ -4,9 +4,10 @@ set -e
 source .env
 
 NETWORK_FOLDER="$1" 
-FROM_SC="$2"
-DEPLOY_SC="$3"
-DEPLOYED_FILENAME="$4"
+CHAIN_ID="$2" 
+FROM_SC="$3"
+DEPLOY_SC="$4"
+DEPLOYED_FILENAME="$5"
 
 mkdir -p deployments/$NETWORK_FOLDER
 
@@ -15,9 +16,9 @@ mkdir -p deployments/$NETWORK_FOLDER
 abi=$(jq -c "{abi: .abi}" ./out/$FROM_SC.sol/$FROM_SC.json)
 address=$(jq -c \
     "{address: .transactions[0].contractAddress}" \
-    ./broadcast/$DEPLOY_SC.s.sol/1/run-latest.json)
+    ./broadcast/$DEPLOY_SC.s.sol/$CHAIN_ID/run-latest.json)
 blocknumberhex=$(jq -rc \
     ".receipts[0].blockNumber" \
-    ./broadcast/$DEPLOY_SC.s.sol/1/run-latest.json)
+    ./broadcast/$DEPLOY_SC.s.sol/$CHAIN_ID/run-latest.json)
 blocknumber=$(cast --to-base $blocknumberhex 10)
 echo "$abi $address {\"blockNumber\": $blocknumber}" | jq -s add >./deployments/$NETWORK_FOLDER/$DEPLOYED_FILENAME.json
